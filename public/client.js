6a3e3e315257d658a8f40bed001c45d69865ea42
Xuerui Fa <faxuerui@seas.upenn.edu>
2016-12-15 16:09:05 -0500

    Added ding to cell clicks

--- public/client.js
+++ public/client.js
@@ -65,7 +65,7 @@ socket.on('update-turn', function (room) {
 });
 
 socket.on('color-cell', function (r, c, turn) {
-	var audio = Audio("stuffed-and-dropped.mp3");
+	var audio = new Audio("stuffed-and-dropped.mp3");
 	audio.play();
 	var $cell = $('.cell');
 	$cell.each(function () {
